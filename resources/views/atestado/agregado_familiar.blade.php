27a9090b3d110c1879a43cb7010214f601262454
janir <janiralves10@gmail.com>
2026-02-10 23:21:44 -0100

    fix

--- resources/views/atestado/agregado_familiar.blade.php
+++ resources/views/atestado/agregado_familiar.blade.php
@@ -26,8 +26,7 @@
     <p> O presente atestado serve para efeito de <strong>{{$assinatura->efeito ?? ''}}</strong>.</p> 
 
     <p> Por ser verdade e me ter sido pedido, mandei passar o presente atestado que 
-        vai por mim assinado e devidamente autenticado com o código de barra 
-        @{{$assinatura->codBarra}}@ em uso
+        vai por mim assinado e devidamente autenticado com o código de barra {{ $assinatura->codBarra ?? '' }} em uso
          nesta Instituição.</p>
 
     <p>
