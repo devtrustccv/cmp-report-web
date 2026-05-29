cafc1448c818fa254db7dbe2ea5d34a56c37a47c
janir <janiralves10@gmail.com>
2026-05-29 17:15:03 -0100

    fix report

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
-    CompraVendaController,
-    IupRemForoController,
     AtestadoController,
-    DoacaoController,
-    PartilhaController,
-    TerrenoController,
     DocumentController,
     CertidaoMatricialController,
     IupController
 };
 
+
+/**
+ * essas são reports para iup ou iti
+ */
 Route::prefix('/')->group(function () {
 
-    Route::get('compra-venda/{id}', [IupController::class, 'getCompraVenda']);
-    Route::get('iupremforo/{id}', [IupController::class, 'getRemForo']);
-    Route::get('iuppartilha/{id}', [IupController::class, 'getPartilha']);
-    Route::get('iupdoacao/{id}', [IupController::class, 'getDoacao']);
-    Route::get('iupterreno/{id}', [IupController::class, 'getTerreno']);
+    Route::get('compra-venda/{id}', [IupController::class, 'compraVenda']);
+    Route::get('iupremforo/{id}', [IupController::class, 'remForo']);
+    Route::get('iuppartilha/{id}', [IupController::class, 'partilha']);
+    Route::get('iupdoacao/{id}', [IupController::class, 'doacao']);
+    Route::get('iupterreno/{id}', [IupController::class, 'terreno']);
     Route::get('document-link-public/{id}', [DocumentController::class, 'loadDocument']);
     Route::get('reports/certidao-matricial/{id}', [CertidaoMatricialController::class, 'gerarCertidaoMatricial']);
     
