5fea4ca1acf8522fb960afab00c270205ff0d67a
janir <janiralves10@gmail.com>
2026-08-13 23:54:54 -0100

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 use Carbon\Carbon;
@@ -22,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('pt');
+
+        if (! $this->app->environment('local')) {
+            URL::forceScheme('https');
+        }
     }
 }
