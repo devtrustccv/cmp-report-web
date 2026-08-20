d08063da6c10927e003d8e9a6dcf6c65fad5eaf7
janir <janiralves10@gmail.com>
2026-08-20 11:07:44 -0100

    fix. report

--- app/Models/SucessorioDto.php
+++ app/Models/SucessorioDto.php
@@ -10,39 +10,55 @@ class SucessorioDto extends Model
     public $timestamps = false;
     protected $table = null;
 
-    // Atributos que o Model pode ter
+    public $duc;
+    public $id;
+    public $matriz;
+    public $fraccao;
+    public $superficie;
+    public $antigosProprietarios;
+    public $tipoTransmissao;
+    public $data_emissao;
+    public $numTransmissao;
+    public $novosProprietarios;
+    public $ducComprador;
     public $multa;
     public $juro;
-    public $total_pago;
-    public $valor_avalidado;
-    public $valor_declarado;
-    public $area;
-    public $comprador;
-    public $vendedor;
-    public $descricao;
-    public $duc;
-    public $matriz;
+    public $dtEscritura;
+    public $ducVendedor;
+    public $dividaComprador;
+    public $total;
+    public $totalExtenso;
+    public $dividaDevedor;
+    public $valorInicial;
+    public $valorFinal;
+    public $valorTransaccao;
+    public $isencao;
+    public $impAPagar;
+    public $cobrado_por;
+    public $data_pagamento;
+    public $norte;
+    public $sul;
+    public $este;
+    public $oeste;
+    public $descMatriz;
+    public $extenso;
     public $local;
     public $numero_processo;
-    public $data_emissao;
-    public $data_pagamento;
-    public $emitido_por;
-    public $cobrado_por;
-    public $estado;
-    public $titulo;
-    public $extenso;
-    public $codigoBarra;
+    public $utilizadorTrm;
+    public $dataEmissaoRequisicao;
     public $meioPagamento;
+    public $estado;
+    public $codigoBarra;
+    public $anoEscritura;
+    public $titulo;
     public $tipoDuc;
-    public $totalExtenso;
-    public $dividaComprador;
 
 
-    // Construtor para popular o model direto com array (ex: resposta da API)
+    // Construtor para popular via JSON ou array
     public function __construct(array $attributes = [])
     {
         foreach ($attributes as $key => $value) {
-            if(property_exists($this, $key)){
+            if (property_exists($this, $key)) {
                 $this->$key = $value;
             }
         }
