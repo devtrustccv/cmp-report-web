6611ccf27ce70e3d77d8f6b6bd509cd872f890f7
janir <janiralves10@gmail.com>
2026-09-21 15:12:12 -0100

    fix

--- app/Http/Controllers/IupController.php
+++ app/Http/Controllers/IupController.php
@@ -59,10 +59,10 @@ class IupController extends Controller
     ) {
         try {
 
-           //$values = $this->cryptoService->decrypt($params);
+           $values = $this->cryptoService->decrypt($params);
 
-            $id = 1; //(int) ($values['id'] ?? 0);
-            $isVerificacao = 1; //(int) ($values['verificacao'] ?? 3); 
+            $id = (int) ($values['id'] ?? 0);
+            $isVerificacao = (int) ($values['verificacao'] ?? 3); 
 
        
 
